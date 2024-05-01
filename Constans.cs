2aa5ead045eedff9cb670f482a1099916d1bbb19
danielgrish <162367520+danielgrish@users.noreply.github.com>
2024-05-01 10:10:45 +0200

    Add files via upload

---
+++ Constans.cs
@@ -0,0 +1,9 @@
+﻿namespace MyApi
+{
+    public class Constans
+    {
+        public static string Address = "https://open-weather13.p.rapidapi.com/city/kyiv/EN";
+        public static string ApiKey = "223e834170msh1c33113d0dba5cep1fb487jsn4d87a691c84d";
+        public static string ApiHost = "open-weather13.p.rapidapi.com";
+    }
+}
